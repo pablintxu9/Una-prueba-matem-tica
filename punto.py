ad4fc2009bad353d59c9412846c7761523771c74
pablo <pablo.diazgrenufiguerolaferrtt@usp.ceu.es>
2025-10-24 11:51:35 +0200

    rectangulo

--- punto.py
+++ punto.py
@@ -9,3 +9,16 @@ class Punto:
 
     def __str__(self):
         return f"Punto({self.x}, {self.y})"
+    
+class rectangulo:
+    def __init__(self, punto_inferior_izquierdo, punto_superior_derecho):
+        self.punto_inferior_izquierdo = punto_inferior_izquierdo
+        self.punto_superior_derecho = punto_superior_derecho
+
+    def area(self):
+        ancho = self.punto_superior_derecho.x - self.punto_inferior_izquierdo.x
+        alto = self.punto_superior_derecho.y - self.punto_inferior_izquierdo.y
+        return ancho * alto
+
+    def __str__(self):
+        return f"Rectangulo({self.punto_inferior_izquierdo}, {self.punto_superior_derecho})"
